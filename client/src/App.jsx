396fe4612fff52c5e2f59c4a5cac9c366518cfd2
chamika <chamikapiyumalee@gmail.com>
2026-08-21 22:55:03 +0530

    feat: connect app shell and board

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Board from "./components/Board";
+import AuthForm from "./components/AuthForm";
 import "./App.css";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Board />} />
-        
+        <Route path="/login" element={<AuthForm />} />
+        <Route path="/register" element={<AuthForm />} />
       </Routes>
     </BrowserRouter>
   );
